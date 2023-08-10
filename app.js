480e5e1489f9b3915e710b1adb91802c432fad79
Kalana Geesara <69765394+KalanaEdirisinghe@users.noreply.github.com>
2023-08-10 10:11:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/data', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data.json'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
